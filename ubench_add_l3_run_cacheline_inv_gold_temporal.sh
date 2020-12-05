4d1f1d5168559a153fec9f349b9e451e49778920
msasongko17 <msasongko17@ku.edu.tr>
2020-12-06 01:19:59 +0300

    updated gold script

--- ubench_add_l3_run_cacheline_inv_gold_temporal.sh
+++ ubench_add_l3_run_cacheline_inv_gold_temporal.sh
@@ -2,7 +2,7 @@
 
 ReuseTracker_path=/home/msasongko17/reusetracker/hpctoolkit-bin
 
-HPCRUN_WP_REUSE_PROFILE_TYPE="SPATIAL" HPCRUN_PROFILE_L3=true HPCRUN_WP_REUSE_BIN_SCHEME=4000,2 HPCRUN_WP_CACHELINE_INVALIDATION=true OMP_NUM_THREADS=32 HPCRUN_THREAD_LOCALITY_MAPPING=0%2%4%6%8%10%12%14%16%18%20%22%24%26%28%30#1%3%5%7%9%11%13%15%17%19%21%23%25%27%29%31 /usr/bin/time -f "Elapsed Time , %e, system, %S, user, %U, memory, %M" $ReuseTracker_path/bin/hpcrun --mapping 0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31 -e WP_REUSETRACKER -e MEM_LOAD_UOPS_RETIRED.L2_MISS@100000 -e MEM_UOPS_RETIRED:ALL_STORES@100000 -o ubench_add_l3_output write-volume/ubench_add -n 32 -s 1.0 -f 0.0 -i 100000000 2>&1 | tee ubench_add_reusetracker_log
+HPCRUN_WP_REUSE_PROFILE_TYPE="TEMPORAL" HPCRUN_PROFILE_L3=true HPCRUN_WP_REUSE_BIN_SCHEME=4000,2 HPCRUN_WP_CACHELINE_INVALIDATION=true OMP_NUM_THREADS=32 HPCRUN_THREAD_LOCALITY_MAPPING=0%2%4%6%8%10%12%14%16%18%20%22%24%26%28%30#1%3%5%7%9%11%13%15%17%19%21%23%25%27%29%31 /usr/bin/time -f "Elapsed Time , %e, system, %S, user, %U, memory, %M" $ReuseTracker_path/bin/hpcrun --mapping 0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31 -e WP_REUSETRACKER -e MEM_LOAD_RETIRED.L2_MISS@10000 -e MEM_INST_RETIRED.ALL_STORES@100000 -o ubench_add_l3_output write-volume/ubench_add -n 32 -s 1.0 -f 0.0 -i 100000000 2>&1 | tee ubench_add_reusetracker_log
 
 #-e MEM_UOPS_RETIRED:ALL_STORES@100000
 #-e MEM_LOAD_UOPS_RETIRED.L2_MISS@100000
